9460ceb279db9b4d7553be2a707bc19b42ff01d7
dismaswidyanto <63232560+dismaswidyanto@users.noreply.github.com>
2022-02-15 12:23:30 +0700

    Update soal-01.c

--- soal-01.c
+++ soal-01.c
@@ -11,13 +11,115 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
+#include <stdio.h>
+#include <stdlib.h>
+
+void main() {
+    int A; //Variable Awal
+    int B; //Variable Akhir
 	
-	//template output
-	printf("\nInput salah. Program selesai!");
-	printf("\nBanyaknya bilangan ganjil : ...", ...);
-	printf("\nBanyaknya bilangan genap : ...", ...);
-	printf("\nBanyaknya bilangan prima : ...", ...);
+    int select; //Variable menentukan ganjil, genap, atau prima 
+    int not_prime; //Untuk menentukan prime atau tidak
 	
-	return 0;
+    int isvalid = 0; //Variable bool untuk validasi A < B
+    int counter = 0 ;
+    
+    printf("PROGRAM PENCARIAN BILANGAN\n");
+    printf("\n");
+	
+	printf("Bilangan apa yang ingin dicari?\n");
+	printf("1 : Ganjil\n");
+	printf("2 : Genap\n");
+	printf("3 : Prima\n\n");
+	
+	printf("Silahkan masukkan input : ");
+	scanf("%d", &select);
+	
+	if(select == 1){
+		printf("\nPROGRAM PENCARIAN BILANGAN GANJIL\n\n");
+	}
+	
+	else if(select == 2){
+		printf("\nPROGRAM PENCARIAN BILANGAN GENAP\n\n");
+	}
+
+	else if(select == 3){
+		printf("\nPROGRAM PENCARIAN BILANGAN PRIMA\n\n");
+	}
+	
+	else{
+		printf("\nInput salah. Program selesai!");
+		return;
+	}
+	
+	//Input angka pengulangan sampai benar
+	while(isvalid == 0){
+		printf("Masukkan bilangan awal mulai : ");
+		scanf("%d", &A);
+		printf("Masukkan bilangan akhir : ");
+		scanf("%d", &B);
+		
+		//Kondisi Input Salah
+		if (A>B){
+			printf("Input salah. Masukkan input kembali!\n\n");
+		}
+		//Kondisi Input Benar
+		else{
+			isvalid =1;
+			printf("\n");
+			if(select == 1){
+				printf("Semua bilangan ganjil dari %d sampai %d adalah :\n", A, B);
+			}
+			
+			if(select == 2){
+				printf("Semua bilangan genap dari %d sampai %d adalah :\n", A, B);
+			}
+			
+			if(select == 3){
+				printf("Semua bilangan prima dari %d sampai %d adalah :\n", A, B);
+			}
+		}
+	}
+	
+	if(select == 1){
+		for(int i = A; i <= B; i++){
+			if (i % 2 == 1 && i >= 0){
+				printf("%d\n", i);
+				counter++;
+			}
+		}
+		printf("\nBanyaknya bilangan ganjil : %d", counter);
+	}
+	
+	if(select == 2){
+		for(int i = A; i <= B; i++){
+			if (i % 2 == 0  && i >= 0){
+				printf("%d\n", i);
+				counter++;
+			}
+		}
+		printf("\nBanyaknya bilangan genap : %d", counter);
+	}
+	
+	if(select == 3){
+		while (A<=B) {
+			not_prime = 0;
+			for (int i = 2; i <= A / 2; ++i) { //Fungsi menentukan prima
+				if (A % i == 0) {
+					not_prime = 1;
+				}
+			}
+			//Jika prima dan lebih dari 1
+			if ((not_prime == 0) && (A > 1)){
+				printf("%d\n", A); 
+				counter++;
+				}
+			
+			++A; 
+		}
+    printf("\nBanyaknya bilangan prima : %d", counter);
+    return;
+	}
+	
+    return;
 }
